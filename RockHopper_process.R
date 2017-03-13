d49f2ff33086dacb01513188d02b453c61d72d30
YulongNiu <niuylscu@gmail.com>
2017-03-13 21:11:44 +0800

    add process wig

--- RockHopper_process.R
+++ RockHopper_process.R
@@ -31,3 +31,29 @@ smumerge <- merge(smugff, smuannot, by.x = 'loc', by.y = 'Location')
 
 save(smumerge, file = '/home/Yulong/RESEARCH/SongYing_MJ201409021010/Rockhopper_Results/smumerge.RData')
 ######################################################################
+
+############################map wig############################
+MapWig <- function(wig, start, end) {
+  ## INPUT: 'wig' is a numeric vector. 'start' and 'end' indicate the start and end position.
+  ## OUTPUT: the count number
+
+  return(sum(wig[start:end]))
+}
+
+read.wig <- function(wigpath) {
+  wig <- read.table(wigpath, skip = 2, stringsAsFactors = FALSE, header = FALSE)
+  return(wig[, 1])
+}
+
+setwd('/home/Yulong/RESEARCH/SongYing_MJ201409021010/Rockhopper_Results/genomeBrowserFiles/')
+
+library('magrittr')
+library('foreach')
+library('doMC')
+
+filename <- dir(pattern = 'v1')
+tmp1 <- foreach (i = 1:length(filename), .combine = cbind) %do% {
+  return(read.wig(filename[i]))
+}
+
+###############################################################
