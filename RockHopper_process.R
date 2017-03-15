27547cb0ecf41affeb67178a158932d4c9c151b5
YulongNiu <niuylscu@gmail.com>
2017-03-15 21:10:19 +0800

    add gene names

--- RockHopper_process.R
+++ RockHopper_process.R
@@ -66,8 +66,8 @@ deg <- read.csv('NC_004350_transcripts.txt', sep = '\t')
 deg <- merge(smumerge, deg, by.x = 'Synonym', by.y = 'Synonym')
 
 deg$log2FC <- log2((deg[, 'RPKM.2'] + 1)/(deg[, 'RPKM.1'] + 1))
-deg <- deg[, c(1, 26, 6, 7, 13, 15, 19, 28:30, 34, 36:38, 42, 46, 44, 45)]
-colnames(deg) <- c('GeneID', 'GeneName', 'Start', 'End', 'Product', 'Length', 'COG',paste0('RawCountControl', 1:3), 'RPKMControl', paste0('RawCountMutant', 1:3), 'RPKMMutant', 'log2FC', 'p-value', 'q-value')
+deg <- deg[, c(1, 26, 12, 6, 7, 13, 15, 19, 28:30, 34, 36:38, 42, 46, 44, 45)]
+colnames(deg) <- c('GeneID', 'Symbol', 'Names', 'Start', 'End', 'Product', 'Length', 'COG',paste0('RawCountControl', 1:3), 'RPKMControl', paste0('RawCountMutant', 1:3), 'RPKMMutant', 'log2FC', 'p-value', 'q-value')
 
 write.csv(deg, 'deg.csv')
 #############################################################
@@ -83,10 +83,9 @@ library('pheatmap')
 
 setwd('/home/Yulong/RESEARCH/SongYing_MJ201409021010/Rockhopper_Results')
 
-
 ##~~~~~~~~~~~~~~~~~~~~~~~~build target and DEGlist object~~~~~~~~~~~~~~
 deg <- read.csv('deg.csv', row.names = 1, stringsAsFactor = FALSE)
-htCountSelect <- deg[, c(8:10, 12:14)]
+htCountSelect <- deg[, c(9:11, 13:15)]
 rownames(htCountSelect) <- deg[, 1]
 
 targets <- data.frame(Group = factor(c('WT', 'WT', 'WT', 'Mutant', 'Mutant', 'Mutant')), Sample = paste0('smu', 1:6))
@@ -105,9 +104,53 @@ resRaw <- results(glioPR)
 resRaw[, 2] <- -resRaw[, 2]
 summary(resRaw)
 res <- cbind(as.matrix(mcols(glioPR)[, 1:10]), assay(rld))
-anno <- deg[match(rownames(res), deg[, 1]), 1:7]
+anno <- deg[match(rownames(res), deg[, 1]), 1:8]
 res <- cbind(anno, res[, 11:16], data.frame(resRaw[, c(5, 6, 2)]))
 write.csv(res, file = '/home/Yulong/RESEARCH/SongYing_MJ201409021010/Rockhopper_Results/degseq2.csv')
-resSig <- res[order(as.numeric(res[, 'qval'])), ]
+resSig <- res[order(as.numeric(res[, 'padj'])), ]
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~heat map~~~~~~~~~~~~~~~~~~~~~~~~~~
+topNum <- 200
+heatmapCount <- resSig[1:topNum, 9:14]
+heatmapCount <- apply(heatmapCount, 1:2, as.numeric)
+rownames(heatmapCount) <- resSig[1:topNum, 'Names']
+
+annoCol <- data.frame(Group = colData(glioPR)[, 1])
+row.names(annoCol) <- rownames(colData(glioPR))
+annoColor <- list(Group = c(WT = '#00C19F', Mutant = '#F8766D'))
+## annoRow = data.frame(GeneClass = factor(rep(c("Path1", "Path2", "Path3"), c(30, 30, 40))))
+## rownames(annoRow) <- rownames(heatmapCount)
+pdf('/home/Yulong/RESEARCH/SongYing_MJ201409021010/Rockhopper_Results/DESeq2_heatmap_top200.pdf')
+pheatmap(heatmapCount, annotation_col = annoCol, annotation_colors = annoColor, fontsize=12, fontsize_row=2, annotation_legend = TRUE)
+dev.off()
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~heatmap~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library("RColorBrewer")
+library("gplots")
+select <- order(rowMeans(counts(glioPR, normalized=TRUE)),decreasing=TRUE)[1:30]
+hmcol <- colorRampPalette(brewer.pal(9, "GnBu"))(100)
+heatmap.2(counts(glioPR,normalized=TRUE)[select,], col = hmcol, Rowv = FALSE, Colv = FALSE, scale="none", dendrogram="none", trace="none", margin=c(10,6))
+heatmap.2(assay(rld)[select,], col = hmcol, Rowv = FALSE, Colv = FALSE, scale="none", dendrogram="none", trace="none", margin=c(10, 6))
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PCA~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+pca <- prcomp(t(assay(rld)))
+percentVar <- pca$sdev^2/sum(pca$sdev^2)
+percentVar <- round(100 * percentVar)
+pca1 <- pca$x[,1]
+pca2 <- pca$x[,2]
+pcaData <- data.frame(PC1 = pca1, PC2 = pca2, Group = colData(rld)[, 1], ID = rownames(colData(rld)))
+pdf('/home/Yulong/RESEARCH/SongYing_MJ201409021010/Rockhopper_Results/DESeq2_PCA.pdf')
+groupCol <- c('#00C19F', '#F8766D')
+ggplot(pcaData, aes(x = PC1, y = PC2, colour = Group)) +
+  geom_point(size = 3) +
+  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
+  scale_color_manual(values = groupCol) +
+  geom_dl(aes(label = ID, color = Group), method = 'smart.grid')
+dev.off()
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##################################################################
